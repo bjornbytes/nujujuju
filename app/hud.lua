ddd203fc9a2e8617ea2e35eadaa731460b8412bc
bjorn <tie.372@gmail.com>
2016-02-29 06:29:15 -0800

    Healthbars;

--- app/hud.lua
+++ app/hud.lua
@@ -45,10 +45,42 @@ function hud:bind()
       g.setColor(160, 255, 160)
       g.print(p.juju, 4, 4 + g.getFont():getHeight())
 
+      app.art.heartFrame:setFilter('nearest')
+      app.art.heart:setFilter('nearest')
+      app.art.heartHalf:setFilter('nearest')
+
+      local healthbars = {}
+      healthbars = util.concat(healthbars, { app.context.objects.muju })
+      healthbars = util.concat(healthbars, util.filter(app.context.objects, 'isMinion'))
+      healthbars = util.concat(healthbars, util.filter(app.context.objects, 'isEnemy'))
+
+      util.each(healthbars, function(unit)
+        self:drawHealthbar(unit)
+      end)
+
       return -1000
     end)
 
   return self
+end
+
+function hud:drawHealthbar(unit)
+  g.white(180)
+
+  local size = app.art.heart:getWidth()
+  local inc = size + 2
+  local x = unit.position.x - (inc * (unit.config.maxHealth - 1) / 2)
+  local y = unit.position.y - 80
+
+  for i = 1, unit.config.maxHealth do
+    g.draw(app.art.heartFrame, x, y, 0, 1, 1, size / 2, size / 2)
+
+    if unit.health >= i then
+      g.draw(app.art.heart, x, y, 0, 1, 1, size / 2, size / 2)
+    end
+
+    x = x + inc
+  end
 end
 
 return hud
