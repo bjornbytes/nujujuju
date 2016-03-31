eadaaae5850722fdb094538b71f719da20200b9f
bjorn <tie.372@gmail.com>
2016-03-30 19:46:21 -0700

    This is cool;

--- app/shaders/godray.glsl
+++ app/shaders/godray.glsl
@@ -6,14 +6,15 @@ extern float time;
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
   vec4 result = Texel(texture, texture_coords);
 
-  float direction = atan((texture_coords.y - .5) / (texture_coords.x - .5));
-  float distance = length(texture_coords - vec2(.5, .5)) * 7;
+  float direction = atan(texture_coords.y - .5, texture_coords.x - .5);
 
-  vec2 rayCoords = vec2(((texture_coords.x - .5) / 2) + .5, ((texture_coords.y - .5) / 2) + .5);
-  rayCoords.x += cos(time / 11 + texture_coords.y / 2) * .04;
-  rayCoords.y += sin(time / 9 + texture_coords.x / 2) * .04;
-  vec4 rayColor = Texel(texture, rayCoords);
-  rayColor.a = .5;
+  float xfactor = 1.2 + cos(texture_coords.y * 100 + time / 10) / 20;
+  float yfactor = 1.2 + cos(texture_coords.x * 100 + time / 10) / 20;
+  xfactor *= .25 + abs(cos(direction * 30));
+  yfactor *= .25 + abs(cos(direction * 30));
+  vec2 rayCoords = vec2(((texture_coords.x - .5) / xfactor) + .5, ((texture_coords.y - .5) / yfactor) + .5);
+  vec4 rayColor = Texel(texture, rayCoords) / 1;
+  rayColor.a /= 2;
 
   return result + rayColor;
 }
