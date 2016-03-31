e142bedc5d6fca869cf92324557fd0cb61657bcd
bjorn <tie.372@gmail.com>
2016-03-30 20:33:47 -0700

    Tuning;

--- app/shaders/godray.glsl
+++ app/shaders/godray.glsl
@@ -10,10 +10,10 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
   float ox = texture_coords.x - .5;
   float oy = texture_coords.y - .5;
 
-  float xfactor = .7 + cos(ox * 100 + time / 91) / 20;
-  float yfactor = .7 + sin(oy * 100 + time / 111) / 20;
-  xfactor *= 1 + abs(cos(time / 43 + (11 + cos(direction) * 5))) / 1.25;
-  yfactor *= 1 + abs(cos(.25 + time / 51 + (11 + sin(direction) * 5))) / 1.25;
+  float xfactor = .7 + cos(ox * 100 + time / 59) / 20;
+  float yfactor = .7 + sin(oy * 100 + .1 + time / 53) / 20;
+  xfactor *= 1 + abs(cos(time / 31 + (11 + cos(direction) * 5))) / 1.5;
+  yfactor *= 1 + abs(cos(.25 + time / 37 + (11 + sin(direction) * 5))) / 1.5;
   float factor = xfactor / 2 + yfactor / 2;
   vec2 rayCoords = vec2(((texture_coords.x - .5) / factor) + .5, ((texture_coords.y - .5) / factor) + .5);
   vec4 rayColor = Texel(texture, rayCoords) / 1;
