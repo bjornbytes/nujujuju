eb3165d73ed666ceac8304e8f2384e78422672c6
bjorn <tie.372@gmail.com>
2016-03-30 20:18:13 -0700

    Think I'm gonna go with this;

--- app/shaders/godray.glsl
+++ app/shaders/godray.glsl
@@ -13,7 +13,7 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
   float xfactor = .7 + cos(ox * 100 + time / 50) / 20;
   float yfactor = .7 + sin(oy * 100 + time / 50) / 20;
   xfactor *= 1 + abs(cos(time / 50 + direction * 3)) / 1;
-  yfactor *= 1 + abs(sin(.25 + time / 50 + direction * 3)) / 1;
+  yfactor *= 1 + abs(sin(time / 50 + direction * 3)) / 1;
   float factor = xfactor / 2 + yfactor / 2;
   vec2 rayCoords = vec2(((texture_coords.x - .5) / factor) + .5, ((texture_coords.y - .5) / factor) + .5);
   vec4 rayColor = Texel(texture, rayCoords) / 1;
